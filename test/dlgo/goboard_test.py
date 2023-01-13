38f704738730c1b156d51cd7928df018cf3765e2
Nikola Kadovic <kadovicnikola@gmail.com>
2023-01-12 20:38:49 -0500

    added more tests

--- test/dlgo/goboard_test.py
+++ test/dlgo/goboard_test.py
@@ -1,7 +1,8 @@
 import pytest
 from test.helpers import is_equal_unordered
 from dlgo.gotypes import Point, Player
-from dlgo.goboard import Move, GoString, Board, GameState  # TODO: reference everything in terms of goboard
+# TODO: reference everything in terms of goboard
+from dlgo.goboard import Move, GoString, Board, GameState
 from dlgo.zobrist import *
 
 
@@ -173,4 +174,146 @@ class TestBoard:
 
 # TODO
 class TestGameState:
-    pass
+    @pytest.fixture(scope="function")
+    def game_state(self) -> GameState:
+        # returns new GameState with 4x4 board
+
+        return GameState.new_game(board_size=4)
+
+    @pytest.fixture(scope="function")
+    def game_state_resign(self) -> GameState:
+        # returns a new GameState, last move is a resignation
+
+        game_state = GameState.new_game(board_size=4)
+        return game_state.apply_move(Move.resign_turn())
+
+    @pytest.fixture(scope="function")
+    def game_state_pass(self) -> GameState:
+        # returns a new GameState, last move is a pass
+
+        game_state = GameState.new_game(board_size=4)
+        return game_state.apply_move(Move.pass_turn())
+
+    @pytest.fixture(scope="function")
+    def game_state_play(self) -> GameState:
+        # returns a new GameState, last move is a play: Point(1, 1)
+
+        game_state = GameState.new_game(board_size=4)
+        return game_state.apply_move(Move.play(Point(1, 1)))
+
+    @pytest.fixture(scope="function")
+    def game_state_self_capture(self) -> GameState:
+        # returns a game_state s.t. placing a stone at point(2, 1) will violate self-capture rule
+        """
+        Final board position: 
+
+        - - - - - -
+        |   A B C D
+        | 1 o o
+        | 2   x o
+        | 3 o o
+        | 4
+        """
+
+        game_state = GameState.new_game(board_size=4)
+        return GameState.apply_sequence(
+            init_state=game_state,
+            moves=[
+                Move.play(Point(2, 2)),
+                Move.play(Point(2, 3)),
+                Move.pass_turn(),
+                Move.play(Point(1, 2)),
+                Move.pass_turn(),
+                Move.play(Point(3, 2)),
+                Move.pass_turn(),
+                Move.play(Point(1, 1)),
+                Move.pass_turn(),
+                Move.play(Point(3, 1))
+            ]
+        )
+
+    @pytest.fixture(scope="function")
+    def game_state_ko(self) -> GameState:
+        # an example of situational super-ko taken from the book.
+        # returns a GameState s.t. playing Point(x, x) violates situational super-ko
+        """
+        Final board position:
+
+        - - - - - - -
+        |   A B C D E 
+        | 1   o
+        | 2 o x o
+        | 3 x   x
+        | 4   x
+        | 5
+
+        The previous board position is the following:
+
+        - - - - - - -
+        |   A B C D E 
+        | 1   o
+        | 2 o   o
+        | 3 x o x
+        | 4   x
+        | 5
+
+        Thus, if black plays Point(3, 2), it should violate ko by bringing the board back to a previous position
+        """
+        game_state = GameState.new_game(board_size=5)
+
+        return GameState.apply_sequence(
+            init_state=game_state,
+            moves=[
+                Move.play(Point(1, 2)),
+                Move.play(Point(4, 2)),
+                Move.play(Point(2, 1)),
+                Move.play(Point(3, 1)),
+                Move.play(Point(2, 3)),
+                Move.play(Point(3, 3)),
+                Move.play(Point(3, 2)),
+                Move.play(Point(2, 2))
+            ]
+        )
+
+    def test_game_state_has_proper_attributes(self, game_state):
+        assert hasattr(game_state, "apply_move")
+        assert hasattr(game_state, "is_valid_move")
+        assert hasattr(game_state, "is_over")
+
+    def test_can_play_move(self, game_state):
+        game_state = game_state.apply_move(Move.play(Point(1, 1)))
+
+        assert game_state.board.is_point_occupied(Point(1, 1))
+
+    def test_can_pass(self, game_state):
+        game_state = game_state.apply_move(Move.pass_turn())
+
+        assert game_state.last_move.pass_turn
+        assert game_state.last_move.point is None
+
+    def test_can_resign(self, game_state):
+        game_state = game_state.apply_move(Move.resign_turn())
+
+        assert game_state.last_move.is_resign
+        assert game_state.last_move.point is None
+
+    def test_valid_move_when_game_over(self, game_state_resign):
+        # if a player has resigned, any move shouldn't be valid
+
+        assert game_state_resign.is_valid_move(Move.pass_turn()) is False
+        assert game_state_resign.is_valid_move(Move.resign_turn()) is False
+        assert game_state_resign.is_valid_move(Move.play(Point(1, 2))) is False
+
+    def test_invalid_move_when_playing_occupied_point(self, game_state_play):
+        # if a point has already been occupied, trying to play on it should be invalid.
+
+        assert not game_state_play.is_valid_move(Move.play(Point(1, 1)))
+
+    def test_cannot_self_capture(self, game_state_self_capture):
+
+        assert not game_state_self_capture.is_valid_move(
+            Move.play(Point(2, 1)))
+
+    def test_cannot_violate_ko(self, game_state_ko):
+
+        assert not game_state_ko.is_valid_move(Move.play(Point(3, 2)))
